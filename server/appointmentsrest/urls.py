8543c7dbbe18614fe64b57aa9e307e5111a3d5f5
karpatxnegen <istvan.karpati@negentropics.com>
2024-11-18 23:15:25 +0100

    delete extra lines from urls.py

--- server/appointmentsrest/urls.py
+++ server/appointmentsrest/urls.py
@@ -4,12 +4,6 @@ from ninja import NinjaAPI
 
 api = NinjaAPI()
 
-
-@api.get("/add")
-def add(request, a: int, b: int):
-    return {"result": a + b}
-
-
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/", api.urls),
